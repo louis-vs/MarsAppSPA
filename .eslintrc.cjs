06bc13dde0ecbddf97c08ec9871546c280b1adb6
Louis Van Steene <Louis.VanSteene@softwire.com>
2023-11-15 11:46:39 +0000

    Install and configure helper plugins for emotion

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,9 +10,13 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    '@emotion',
+  ],
   rules: {
     'react/prop-types': [ 0 ],
+    'react/no-unknown-property': [ 'error', { 'ignore': ['css'] } ],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
